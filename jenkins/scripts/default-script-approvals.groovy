4b136a4930536a7f47579ccd374b5ed5cdf6605a
Patrick Liesveld <pliesveld@users.noreply.github.com>
2020-07-13 10:53:24 -0400

    adding script approval java.time.DayOfWeek

--- jenkins/scripts/default-script-approvals.groovy
+++ jenkins/scripts/default-script-approvals.groovy
@@ -1,6 +1,7 @@
 def scriptApproval = org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.get()
 
 String[] signs = [
+    "method java.time.DayOfWeek getValue",
     "method java.time.temporal.Temporal plus long java.time.temporal.TemporalUnit",
     "method java.time.temporal.Temporal with java.time.temporal.TemporalAdjuster",
     "method java.time.temporal.TemporalAccessor get java.time.temporal.TemporalField",
